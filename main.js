f447cdae65dec7a821ba53829220700dfa26ba61
github-devireddyprasanth22 <github-dpkrdrkr@gmail.com>
2023-06-14 00:06:30 +0800

    Cleaning up into directories

--- main.js
+++
@@ -1,3 +0,0 @@
-const MENU_ICON = document.querySelector('.hamburger-menu');
-const NAVBAR = document.querySelector('.navbar');
-MENU_ICON.addEventListener("click",  function(){NAVBAR.classList.toggle("change");});
---
+++ server/main.js
@@ -0,0 +1,5 @@
+const MENU_ICON = document.querySelector(".hamburger-menu");
+const NAVBAR = document.querySelector(".navbar");
+MENU_ICON.addEventListener("click", function () {
+  NAVBAR.classList.toggle("change");
+});
