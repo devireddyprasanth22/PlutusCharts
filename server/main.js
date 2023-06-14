1c5df3bf883a34fb8e605647951bc149ca7019a1
github-devireddyprasanth22 <github-dpkrdrkr@gmail.com>
2023-06-14 11:54:18 +0800

    Position of menu items adjusted, check for empty search introduced

--- server/main.js
+++ server/main.js
@@ -3,3 +3,15 @@ const NAVBAR = document.querySelector(".navbar");
 MENU_ICON.addEventListener("click", function () {
   NAVBAR.classList.toggle("change");
 });
+const TICKER = document.getElementById("stockTicker");
+
+function getStockDetails() {
+  const stock = TICKER.value;
+
+  if (stock.trim() == "") {
+    alert("Please enter a ticker");
+  }
+}
+
+const searchBtn = document.querySelector(".searchbtn");
+searchBtn.addEventListener("click", getStockDetails);
