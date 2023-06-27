9d99253073bcc3279bb2c12a5f495e7380d81b78
github-devireddyprasanth22 <dpkrdrkr@gmail.com>
2023-06-27 17:02:33 +0800

    Name, price and change of ticker  can now be seen on stockPage

--- client/stockPage.js
+++ client/stockPage.js
@@ -1,3 +1,9 @@
 const tickerName = document.getElementById("tickerName");
+const tickerPrice = document.getElementById("tickerPrice");
+const tickerChange = document.getElementById("tickerChange");
+
 let stockData = localStorage.getItem("stockData");
 tickerName.innerHTML = JSON.parse(stockData)["Global Quote"]["01. symbol"];
+tickerPrice.innerHTML = JSON.parse(stockData)["Global Quote"]["05. price"];
+tickerChange.innerHTML =
+  JSON.parse(stockData)["Global Quote"]["10. change percent"];
